0bd294c6d33fbecc647537d43fac96c9179bd9c0
cpolitano <cpolitano8@gmail.com>
2015-06-21 17:09:13 -0400

    built a simple server

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import "net/http"
+
 func main() {
-    println("hello!")
+    http.HandleFunc("/", hello)
+    http.ListenAndServe(":8080", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+    // println("\nhello!\n\n")
+    w.Write([]byte("hello world"))
 }
