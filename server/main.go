21c912c12c92ecd45735499f0c7ddc9382e73159
Clare Hutchings <clareh@socialtables.com>
2017-01-29 10:59:39 -0500

    tcp server

--- server/main.go
+++ server/main.go
@@ -1,15 +1,36 @@
 package main
 
-import "net/http"
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("server running on port 8080")
-	http.HandleFunc("/", hello)
-	http.ListenAndServe(":8080", nil)
+	// fmt.Println("server running on port 8080")
+	// http.HandleFunc("/", hello)
+	// http.ListenAndServe(":8080", nil)
 
-	// _ is blank identifier, representing an error
-	// res, _ := http.Get("http://dev.twitter.com")
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Panic(err)
+	} 
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+
+		io.WriteString(conn, "\nHello from TCP server\n")
+		fmt.Fprintln(conn, "How are you doing?")
+		fmt.Fprintf(conn, "%v", "Well, I hope!\n")
+
+		conn.Close()
+	}
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
