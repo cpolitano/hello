6c8b9d122877f12fa52c1b7c9c783e392ce14d54
Clare Hutchings <clareh@socialtables.com>
2017-02-05 16:01:41 -0500

    routes without mux

--- routing/main.go
+++ routing/main.go
@@ -10,24 +10,17 @@ import (
 // 	ServeHTTP(ResponseWriter, *Request)
 // }
 
-type getBooks int
-type getTapes int
-
-func (b getBooks) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+func getBooks(res http.ResponseWriter, req *http.Request) {
 	io.WriteString(res, "books")
 }
 
-func (t getTapes) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+func getTapes(res http.ResponseWriter, req *http.Request) {
 	io.WriteString(res, "tapes")
 }
 
 func main() {
-	var b getBooks
-	var t getTapes
+	http.HandleFunc("/books/", getBooks) // need trailing slash to handle /books/book_id 
+	http.HandleFunc("/tapes", getTapes)
 
-	mux := http.NewServeMux()
-	mux.Handle("/books", b)
-	mux.Handle("/tapes", t)
-
-	http.ListenAndServe(":8080", mux)
+	http.ListenAndServe(":8080", nil)
 }
