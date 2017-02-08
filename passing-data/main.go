7e1b7ee1acc22d49ba811c1d4c4df4f8c3e173ba
Clare Hutchings <clareh@socialtables.com>
2017-02-08 08:03:20 -0500

    read and save file

--- passing-data/main.go
+++ passing-data/main.go
@@ -4,6 +4,9 @@ import (
 	"io"
 	"net/http"
 	"log"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 )
 
 func getQueryParams(w http.ResponseWriter, req *http.Request) {
@@ -18,8 +21,53 @@ func getQueryParams(w http.ResponseWriter, req *http.Request) {
 	// POST submits data in body, GET submits data in query params
 }
 
+// accepts file and saves to server
+func handleContact(w http.ResponseWriter, req *http.Request) {
+	var s string
+	
+	if req.Method == http.MethodPost {
+		// get file
+		f, h, err := req.FormFile("q")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		} 
+		defer f.Close()
+
+		// read file as byte slice
+		bs, err := ioutil.ReadAll(f)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		} 
+		s = string(bs)
+
+		// save file
+		destination, err := os.Create(filepath.Join("./user/", h.Filename))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		} 
+		defer destination.Close()
+
+		_, err = destination.Write(bs) // writes bytes to file
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		} 
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `
+		<form method="POST" enctype="multipart/form-data">
+		 <input type="file" name="q">
+		 <input type="submit">
+		</form>
+		<br>`+s)
+}
+
 func main() {
 	http.HandleFunc("/", getQueryParams)
+	http.HandleFunc("/contact", handleContact)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	log.Fatal(http.ListenAndServe(":8080", nil)) 
 }
