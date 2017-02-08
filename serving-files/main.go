be5fd351ec54987f010a739a660f2690277bf77d
Clare Hutchings <clareh@socialtables.com>
2017-02-07 19:31:11 -0500

    serve files with StripPrefix

--- serving-files/main.go
+++ serving-files/main.go
@@ -7,11 +7,12 @@ import (
 
 func hedgie(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8" )
-	io.WriteString(w, `<img src="/hedgie.jpg">`)
+	io.WriteString(w, `<img src="/resources/hedgie.jpg">`)
 }
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("."))) // serve all files in .
+	http.Handle("/resources/", http.StripPrefix("/resources", http.FileServer(http.Dir("./assets")))) // serve all files in assets
 	http.HandleFunc("/hedgie", hedgie)
 	http.ListenAndServe(":8080", nil) 
 }
