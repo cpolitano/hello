9b7fb7a89e2f8e30cb94fa9bf904915d47616f1e
Clare Politano Hutchings <cpolitano8@gmail.com>
2019-01-30 08:07:59 -0600

    marshal ex

--- data-structures/json/main.go
+++ data-structures/json/main.go
@@ -6,7 +6,9 @@ import (
 	"os"
 )
 
-// capitalized struct name and field names mean they will be visible and usable outside package
+// capitalized struct name and field names
+// mean they will be visible and usable outside package
+// and inside main.go
 type ColorGroup struct {
 	ID     int
 	Name   string
@@ -22,22 +24,34 @@ func main() {
 
 	// JSON MARSHAL
 	// composite literal to instantiate a ColorGroup
-	group := ColorGroup{
+	group1 := ColorGroup{
 		ID:     1,
 		Name:   "Reds",
 		Colors: []string{"Maroon", "Burgundy", "Oxblood", "Crimson"},
 	}
-
 	// json.Marshal turns the struct into json
-  // Marshal takes any type and returns slice of bytes
-	b, err := json.Marshal(group)
+	// Marshal takes any type and returns slice of bytes
+	b, err := json.Marshal(group1)
 	// always check for errors right after one might be returned
 	// always check for errors.
 	if err != nil {
 		fmt.Println("err: ", err)
 	}
-
 	os.Stdout.Write(b)
+
+	group2 := ColorGroup{
+		ID:     2,
+		Name:   "Yellows",
+		Colors: []string{"Lemon", "Goldenrod", "Sunflower"},
+	}
+
+	groups := []ColorGroup{group1, group2}
+	// returns byte slice
+	bs, err := json.Marshal(groups)
+	if err != nil {
+		fmt.Println("err: ", err)
+	}
+	fmt.Println(string(bs)) // convert byte slice to json string
 
 	// JSON UNMARSHAL
 	// insert raw json string literal into a slice of byte
@@ -47,8 +61,8 @@ func main() {
 	// slice of animals
 	var animals []Animal
 	// unmarshal byte slice and store it at address of animals
-  // Unmarshal takes slice of bytes and stores unmarshaled data in the location pointed to by &animals
-  // Unmarshal ONLY returns an error if anything
+	// Unmarshal takes slice of bytes and stores unmarshaled data in the location pointed to by &animals
+	// Unmarshal ONLY returns an error if anything
 	err = json.Unmarshal(jsonBlob, &animals)
 	if err != nil {
 		fmt.Println("err: ", err)
