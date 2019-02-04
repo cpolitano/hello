0bd7bc626f7854b58fa83e42f6606836c20f8807
Clare Politano Hutchings <cpolitano8@gmail.com>
2019-02-04 10:12:59 -0600

    json string

---
+++ data-structures/json/stringify.js
@@ -0,0 +1,10 @@
+const geoJson = {
+    type: "Polygon",
+    coordinates: [
+        [[30, 10], [40, 40], [20, 40], [10, 20], [30, 10]]
+    ]
+}
+
+console.log(JSON.stringify(geoJson));
+
+const formattedGeoJson = "{type: 'Polygon', coordinates: [[[30, 10], [40, 40], [20, 40], [10, 20], [30, 10]]]}"
