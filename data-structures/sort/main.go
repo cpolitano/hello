87aacb128bb948cc2e327ef3a71753c74510ea62
Clare Politano Hutchings <cpolitano8@gmail.com>
2019-02-07 09:38:35 -0600

    use sort

--- data-structures/sort/main.go
+++ data-structures/sort/main.go
@@ -2,9 +2,18 @@ package main
 
 import (
   "fmt"
+  "sort"
 )
 
 func main() {
   xi := []int{5, 13, 8, 3, 75, 34, 7, 2, 52, 25, 26}
   xs := []string{"Mango", "Papaya", "Aguacate", "Chayote", "Zapote"}
+
+  sort.Ints(xi) // no reassignment needed, changes slice in place
+  // a SLICE is a pointer, length, and capacity
+
+  sort.Strings(xs)
+
+  fmt.Println(xi)
+  fmt.Println(xs)
 }
