dd1c4e7f901e06a76c342f698d63a2b1e44335c9
Clare Politano Hutchings <cpolitano8@gmail.com>
2019-02-10 18:10:11 -0600

    sort float64

--- data-structures/sort/main.go
+++ data-structures/sort/main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
-  "fmt"
-  "sort"
+	"fmt"
+	"sort"
 )
 
 func main() {
-  xi := []int{5, 13, 8, 3, 75, 34, 7, 2, 52, 25, 26}
-  xs := []string{"Mango", "Papaya", "Aguacate", "Chayote", "Zapote"}
+	xi := []int{5, 13, 8, 3, 75, 34, 7, 2, 52, 25, 26}
+	xs := []string{"Mango", "Papaya", "Aguacate", "Chayote", "Zapote"}
+	xf := []float64{5.098, 2.019, 4.00003, 1.9, 1.922, 3.245667}
 
-  sort.Ints(xi) // no reassignment needed, changes slice in place
-  // a SLICE is a pointer, length, and capacity
+	sort.Ints(xi) // no reassignment needed, changes slice in place
+	// a SLICE is a pointer, length, and capacity
 
-  sort.Strings(xs)
+	sort.Strings(xs)
+	sort.Float64s(xf)
 
-  fmt.Println(xi)
-  fmt.Println(xs)
+	fmt.Println(xi)
+	fmt.Println(xs)
+	fmt.Println(xf)
 }
