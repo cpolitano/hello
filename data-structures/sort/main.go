73a948bebfae0ad19f5d74f666e4e28badfe2654
Clare Politano Hutchings <cpolitano8@gmail.com>
2019-02-18 08:27:04 -0600

    sort struct by int

--- data-structures/sort/main.go
+++ data-structures/sort/main.go
@@ -5,6 +5,19 @@ import (
 	"sort"
 )
 
+type person struct {
+	first     string
+	age       int
+	vegetable string
+}
+
+type ByAge []person
+
+// Sort Interface is type interface and takes methods Len, Less, and Swap
+func (a ByAge) Len() int           { return len(a) }
+func (a ByAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByAge) Less(i, j int) bool { return a[i].age < a[j].age }
+
 func main() {
 	xi := []int{5, 13, 8, 3, 75, 34, 7, 2, 52, 25, 26}
 	xs := []string{"Mango", "Papaya", "Aguacate", "Chayote", "Zapote"}
@@ -19,4 +32,17 @@ func main() {
 	fmt.Println(xi)
 	fmt.Println(xs)
 	fmt.Println(xf)
+
+	// sorting slices and types
+	p1 := person{"James", 33, "beet"}
+	p2 := person{"Pat", 28, "beet"}
+	p3 := person{"Clare", 32, "kale"}
+	p4 := person{"Sara", 30, "broccoli"}
+
+	people := []person{p1, p2, p3, p4}
+	fmt.Println(people)
+
+	// conversion - convert people into type ByAge
+	sort.Sort(ByAge(people))
+	fmt.Println(people)
 }
